dbf5574d01b131f4e73ebb5edbdadb321e4e7642
Eric Matlock <ermatlock@gmail.com>
2021-09-23 22:35:43 -0400

    Add developer model

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  nickName: 'SuperGoogler',
+  devCategory: 'Front End',
+  googleQueries: 56,
+  slackQueries: 10,
+  isExhausted: true,
+  likesSnacks: true,
+  abilities: ['resilience', 'growth mindset', 'SuperLearner', 'logical', 'asks questions'],
+  toolset: ['macbook', 'monitors', 'headphones', 'ChillCow Playlist', 'keyboard', 'comfy chair'],
+};
