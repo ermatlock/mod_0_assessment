951fd66bb299b027f78d666717e296d38f70a8fc
Eric Matlock <ermatlock@gmail.com>
2021-09-23 22:17:30 -0400

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,20 @@
+var movie = 'Tenet';
+console.log(movie)
+
+var isEntertaining = true;
+console.log(isEntertaining)
+
+movie = 'A Quiet Place';
+console.log(movie)
+
+var num4 = 2;
+var num6 = 7;
+var product = num6 * num4;
+console.log(product)
+
+var favoriteFoods = ['pizza', 'steak', 'pad kee mao'];
+favoriteFoods.shift();
+console.log(favoriteFoods)
+
+var charCount = movie.length;
+console.log(charCount)
