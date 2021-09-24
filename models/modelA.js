da4dd27770d60bff6370e93ed3fbdfd73a6ba91d
Eric Matlock <ermatlock@gmail.com>
2021-09-23 22:27:17 -0400

    Add dinosaur model

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  name: 'Velociraptor',
+  period: 'Late Cretacious',
+  lengthMeters: 2,
+  weightKilos: 15,
+  predator: true,
+  herbivore: false,
+  periodFauna: ['Protoceratops', 'Triceratops', 'Hadrosaurus', 'Tyrannosaurus'],
+  periodFlora: ['cycad', 'ginkgo', 'conifer', 'fern'],
+};
